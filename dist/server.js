955caf2656f2c0eef7b6e75eba6d311bca1b1826
Vlad Alive <vlad@rubycluster.com>
2017-05-25 15:44:42 +0000

    [build]

---
+++ dist/server.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var express = require('express'),
+    app = express();
+
+app.use(express.static('./dist/'));
+
+app.all('*', function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+  next();
+});
+
+app.set('port', process.env.PORT || 5000);
+
+app.listen(app.get('port'), function () {
+  console.log('Express server listening on port ' + app.get('port'));
+});
